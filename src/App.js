ae722af7dd3937d0fc50330200484a45207dc768
iminfinity <this.is.me.infinity@gmail.com>
2020-09-02 20:07:10 +0530

    removed draft.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
-import React, { useMemo, useState } from "react";
-
-import { createEditor } from "slate";
-import { Slate, Editable, withReact } from "slate-react";
-
+import React from "react";
 import "./App.css";
 
-const CodeElement = (props) => {
-  return (
-    <pre {...props.attributes}>
-      <code>{props.children}</code>
-    </pre>
-  );
-};
-
+import Notes from "./components/Notes/notes.component";
+import AddNodes from "./components/AddNotes/add-notes.component";
 function App() {
-  const editor = useMemo(() => withReact(createEditor()), []);
-  const [value, setValue] = useState([
-    {
-      type: "paragraph",
-      children: [{ text: "A line of text in a paragraph." }],
-    },
-  ]);
   return (
     <div className="App">
-      <Slate
-        editor={editor}
-        value={value}
-        onChange={(newValue) => setValue(newValue)}
-      >
-        <Editable
-          onKeyDown={(event) => {
-            if (event.key === "&") {
-              event.preventDefault();
-              editor.insertText("and");
-            }
-            console.log(editor);
-            console.log(editor.children[0].children[0].text);
-          }}
-        />
-      </Slate>
+      <Notes />
+      <AddNodes />
     </div>
   );
 }
