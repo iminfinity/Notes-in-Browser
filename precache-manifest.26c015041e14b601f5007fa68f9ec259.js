64e99409d9fabcd71456bb6de42fb4eae52cc2f8
iminfinity <this.is.me.infinity@gmail.com>
2020-09-06 11:18:31 +0530

    Updates

---
+++ precache-manifest.26c015041e14b601f5007fa68f9ec259.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b7cbeb3fd88650814b38ad7551b65b",
+    "url": "/Notes-in-Browser/index.html"
+  },
+  {
+    "revision": "ed07dd0041e0d8c2eeb0",
+    "url": "/Notes-in-Browser/static/css/main.fa22f039.chunk.css"
+  },
+  {
+    "revision": "f1c0993c60c44d77928c",
+    "url": "/Notes-in-Browser/static/js/2.4fcb0a05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Notes-in-Browser/static/js/2.4fcb0a05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed07dd0041e0d8c2eeb0",
+    "url": "/Notes-in-Browser/static/js/main.d9150ad5.chunk.js"
+  },
+  {
+    "revision": "f8a78c5374f18e542f56",
+    "url": "/Notes-in-Browser/static/js/runtime-main.7b15b4af.js"
+  },
+  {
+    "revision": "a9f7ad3dc6d998df5c984f5363913c7f",
+    "url": "/Notes-in-Browser/static/media/github.a9f7ad3d.svg"
+  }
+]);
